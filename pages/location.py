bb5c7f380de3b03d6d92c91bbd59efbc1724aa49
Andrea <36055023+ilvenini@users.noreply.github.com>
2025-02-26 22:24:41 +0100

    agg

--- pages/location.py
+++ pages/location.py
@@ -1,40 +1,32 @@
 import streamlit as st
-from streamlit_folium import folium_static
-import folium
 from modules import utils
 
-# Funzione per ottenere il parametro "id" dall'URL
-def get_query_params():
-    query_params = st.query_params
-    return query_params.get("id", None)
-
 # Pagina 1: Personalizzata per ogni utente
-def app():
-    utils.set_font("Delius Swash Caps")
-    #st.image("savethedate.png")
-    #st.write("## Matrimonio di Annamaria e Andrea")
 
-    
-    st.image("img/bersi1.png")
-    
-    
-    user_id = get_query_params()
-    if user_id and user_id in USER_DATA:
-        st.write(USER_DATA[user_id]["text"], unsafe_allow_html=True)
-    else:
-        st.write(USER_DATA['all']["text"], unsafe_allow_html=True)
-    
-    st.image("img/bersi2.png")
-    
-    if user_id and user_id in USER_DATA:
-        st.write(USER_DATA[user_id]["text2"], unsafe_allow_html=True)
-    else:
-        st.write(USER_DATA['all']["text2"], unsafe_allow_html=True)
-  
-    st.image("img/bersi3.png")
+utils.set_font("Delius Swash Caps")
+#st.image("savethedate.png")
+#st.write("## Matrimonio di Annamaria e Andrea")
+
+st.image("img/bersi1.png")
 
 # Dizionario con messaggi e immagini personalizzate
 USER_DATA = {
     "all": {"text": "Il ricevimento si terrà nell'incantevole cantina **Bersi Serlini**, situata nel cuore della Franciacorta a **Provaglio d\'Iseo** (BS).",
            "text2": "I festeggiamenti inizieranno alle ore **12.30** e dureranno fino alle ore **20.30**.",},
 }
+
+
+user_id = utils.get_query_params()
+if user_id and user_id in USER_DATA:
+    st.write(USER_DATA[user_id]["text"], unsafe_allow_html=True)
+else:
+    st.write(USER_DATA['all']["text"], unsafe_allow_html=True)
+
+st.image("img/bersi2.png")
+
+if user_id and user_id in USER_DATA:
+    st.write(USER_DATA[user_id]["text2"], unsafe_allow_html=True)
+else:
+    st.write(USER_DATA['all']["text2"], unsafe_allow_html=True)
+
+st.image("img/bersi3.png")
--- pages/notes.py
+++ pages/notes.py
@@ -2,50 +2,15 @@ import streamlit as st
 from modules import utils
 import requests
     
-# Pagina 1: Personalizzata per ogni utente
-def app():
-    set_font("Delius Swash Caps")
+
+utils.set_font("Delius Swash Caps")
+
+user_id = utils.get_query_params()
+
+utils.set_font("Delius Swash Caps")
+#st.write("## Matrimonio di Annamaria e Andrea")
 
 
-# Funzione per ottenere il parametro "id" dall'URL
-def get_query_params():
-    query_params = st.query_params
-    return query_params.get("id", None)
-
-# Pagina 1: Personalizzata per ogni utente
-def app():
-    utils.set_font("Delius Swash Caps")
-    #st.write("## Matrimonio di Annamaria e Andrea")
-    
-    user_id = get_query_params()
-    if user_id and user_id in USER_DATA:
-        st.write(USER_DATA[user_id]["text"], unsafe_allow_html=True)
-    else:
-        st.write(USER_DATA['all']["text"], unsafe_allow_html=True)
-
-    TOKEN = st.secrets["BOT_TOKEN"] # BotFather
-    CHAT_ID = st.secrets["CHAT_ID"]
-    
-    def invia_telegram(nome, messaggio):
-        testo = f"📩 **Nuovo Messaggio!**\n👤 Nome: {nome}\n📝 Messaggio: {messaggio}"
-        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-        requests.post(url, data={"chat_id": CHAT_ID, "text": testo, "parse_mode": "Markdown"})
-    
-    # Form Streamlit
-    st.title("Contattaci 📩")
-    with st.form("form_contatto"):
-        nome = st.text_input("Nome", value=user_id)
-        messaggio = st.text_area("Messaggio")
-        submit = st.form_submit_button("Invia")
-    
-    if submit:
-        if nome and messaggio:
-            invia_telegram(nome, messaggio)
-            st.success("Messaggio inviato su Telegram!")
-        else:
-            st.warning("Compila tutti i campi!")
-
-    
 # Dizionario con messaggi e immagini personalizzate
 USER_DATA = {
     "all": {"text": "Per permetterci di organizzare al meglio la festa, vi chiediamo di **confermare la vostra presenza entro il 15 maggio**!<br><br>\
@@ -54,3 +19,32 @@ USER_DATA = {
     iban: it93f0301503200000003641112"},
 }
 
+user_id = get_query_params()
+if user_id and user_id in USER_DATA:
+    st.write(USER_DATA[user_id]["text"], unsafe_allow_html=True)
+else:
+    st.write(USER_DATA['all']["text"], unsafe_allow_html=True)
+
+TOKEN = st.secrets["BOT_TOKEN"] # BotFather
+CHAT_ID = st.secrets["CHAT_ID"]
+
+def invia_telegram(nome, messaggio):
+    testo = f"📩 **Nuovo Messaggio!**\n👤 Nome: {nome}\n📝 Messaggio: {messaggio}"
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": testo, "parse_mode": "Markdown"})
+
+# Form Streamlit
+st.title("Contattaci 📩")
+with st.form("form_contatto"):
+    nome = st.text_input("Nome", value=user_id)
+    messaggio = st.text_area("Messaggio")
+    submit = st.form_submit_button("Invia")
+
+if submit:
+    if nome and messaggio:
+        invia_telegram(nome, messaggio)
+        st.success("Messaggio inviato su Telegram!")
+    else:
+        st.warning("Compila tutti i campi!")
+
+    
