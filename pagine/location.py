622a49cee537c7293ccddd02da04194586d5c0ad
Andrea <36055023+ilvenini@users.noreply.github.com>
2025-02-26 22:46:18 +0100

    a

--- pagine/location.py
+++ pagine/location.py
@@ -40,7 +40,7 @@ def app():
 
     maps_url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.6972540616753!2d10.019785576637224!3d45.63681782190421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478165bef8329e71%3A0xe84fbfd81f60c13b!2sBersi%20Serlini%20Franciacorta!5e0!3m2!1sen!2sit!4v1740606188935!5m2!1sen!2sit"
 
-    st.components.v1.iframe(maps_url, width=700, height=500)
+    st.components.v1.iframe(maps_url, width=200, height=200)
 
 
 # Dizionario con messaggi e immagini personalizzate
