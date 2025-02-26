9c435f1aa63aa9b52d4c5124395291f63f9e6d72
Andrea <36055023+ilvenini@users.noreply.github.com>
2025-02-26 22:45:17 +0100

    a

--- pagine/location.py
+++ pagine/location.py
@@ -33,6 +33,10 @@ def app():
   
     st.image("img/bersi3.png")
 
+    if user_id and user_id in USER_DATA:
+        st.write(USER_DATA[user_id]["text3"], unsafe_allow_html=True)
+    else:
+        st.write(USER_DATA['all']["text3"], unsafe_allow_html=True)
 
     maps_url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.6972540616753!2d10.019785576637224!3d45.63681782190421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478165bef8329e71%3A0xe84fbfd81f60c13b!2sBersi%20Serlini%20Franciacorta!5e0!3m2!1sen!2sit!4v1740606188935!5m2!1sen!2sit"
 
@@ -42,5 +46,6 @@ def app():
 # Dizionario con messaggi e immagini personalizzate
 USER_DATA = {
     "all": {"text": "Il ricevimento si terrà nell'incantevole cantina **Bersi Serlini**, situata nel cuore della Franciacorta a **Provaglio d\'Iseo** (BS).",
-           "text2": "I festeggiamenti inizieranno alle ore **12.30** e dureranno fino alle ore **20.30**.",},
+           "text2": "I festeggiamenti inizieranno alle ore **12.30** e dureranno fino alle ore **20.30**.",
+           "text3": "Le indicazioni per raggiungerci.",},
 }
