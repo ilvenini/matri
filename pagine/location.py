68776f535dd9c92e5368381934cd4a87c04de8ca
Andrea <36055023+ilvenini@users.noreply.github.com>
2025-03-01 18:48:00 +0100

    a

--- pagine/location.py
+++ pagine/location.py
@@ -7,7 +7,7 @@ def app():
     #st.write("## Matrimonio di Annamaria e Andrea")
     user_id = utils.get_query_params()
 
-    st.image("img/comune.jpg", width="100%")
+    st.image("img/comune.jpg", width=450)
 
     st.write(USER_DATA['all']["text1"], unsafe_allow_html=True)
 
