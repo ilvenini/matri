41d1227bb7c4de7276e62a52349b12a7272d33a9
Andrea <36055023+ilvenini@users.noreply.github.com>
2025-03-01 18:09:13 +0100

    mappe

--- pagine/location.py
+++ pagine/location.py
@@ -5,36 +5,40 @@ def app():
     utils.set_font("Delius Swash Caps")
     #st.image("savethedate.png")
     #st.write("## Matrimonio di Annamaria e Andrea")
-
-    st.image("img/bersi1.png")
-    
     user_id = utils.get_query_params()
-    
-    if user_id and user_id in USER_DATA:
-        st.write(USER_DATA[user_id]["text"], unsafe_allow_html=True)
-    else:
-        st.write(USER_DATA['all']["text"], unsafe_allow_html=True)
-    
-    st.image("img/bersi2.png")
-    
-    if user_id and user_id in USER_DATA:
-        st.write(USER_DATA[user_id]["text2"], unsafe_allow_html=True)
-    else:
-        st.write(USER_DATA['all']["text2"], unsafe_allow_html=True)
-  
-    st.image("img/bersi3.png")
 
-    if user_id and user_id in USER_DATA:
-        st.write(USER_DATA[user_id]["text3"], unsafe_allow_html=True)
-    else:
-        st.write(USER_DATA['all']["text3"], unsafe_allow_html=True)
+    st.image("img/comune.jpg")
 
-    maps_url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.6972540616753!2d10.019785576637224!3d45.63681782190421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478165bef8329e71%3A0xe84fbfd81f60c13b!2sBersi%20Serlini%20Franciacorta!5e0!3m2!1sen!2sit!4v1740606188935!5m2!1sen!2sit"
+    st.write(USER_DATA['all']["text1"], unsafe_allow_html=True)
+
+    maps_url1 = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d829.5008035747104!2d10.063414284393087!3d45.59822966865703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47816fef53b5a2a9%3A0x23d07dd3c0e21666!2sPiazza%20Europa%2C%2016%2C%2025050%20Passirano%20BS!5e0!3m2!1sen!2sit!4v1740848655085!5m2!1sen!2sit"
 
     # Iframe responsive con larghezza dinamica
     st.markdown(
         f"""
-        <iframe src="{maps_url}" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        <iframe src="{maps_url1}" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.image("img/bersi1.png")
+    
+    st.write(USER_DATA['all']["text2"], unsafe_allow_html=True)
+    
+    st.image("img/bersi2.png")
+    
+    st.write(USER_DATA['all']["text3"], unsafe_allow_html=True)
+  
+    st.image("img/bersi3.png")
+
+    st.write(USER_DATA['all']["text4"], unsafe_allow_html=True)
+
+    maps_url12 = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.6972540616753!2d10.019785576637224!3d45.63681782190421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478165bef8329e71%3A0xe84fbfd81f60c13b!2sBersi%20Serlini%20Franciacorta!5e0!3m2!1sen!2sit!4v1740606188935!5m2!1sen!2sit"
+
+    # Iframe responsive con larghezza dinamica
+    st.markdown(
+        f"""
+        <iframe src="{maps_url2}" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         """,
         unsafe_allow_html=True
     )
@@ -42,7 +46,8 @@ def app():
 
 # Dizionario con messaggi e immagini personalizzate
 USER_DATA = {
-    "all": {"text": "Il ricevimento si terrà nell'incantevole cantina **Bersi Serlini**, situata nel cuore della Franciacorta a **Provaglio d\'Iseo** (BS).",
-           "text2": "I festeggiamenti inizieranno alle ore **12.30** e dureranno fino alle ore **20.30**.",
-           "text3": "Le indicazioni per raggiungerci.",},
+    "all": {"text1": "La cerimonia nella sala civica del **Comune di Passirano**.",
+            "text2": "Il ricevimento si terrà nell'incantevole cantina **Bersi Serlini**, situata nel cuore della Franciacorta a **Provaglio d\'Iseo** (BS).",
+           "text3": "I festeggiamenti inizieranno alle ore **12.30** e dureranno fino alle ore **20.30**.",
+           "text4": "Le indicazioni per raggiungerci.",},
 }
