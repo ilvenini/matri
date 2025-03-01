c4acbca2434d1f6c05d9d9dd5e746e78b2f806ac
Andrea <36055023+ilvenini@users.noreply.github.com>
2025-03-01 18:16:52 +0100

    mappe

--- pagine/location.py
+++ pagine/location.py
@@ -22,18 +22,16 @@ def app():
     )
 
     #st.image("img/bersi1.png")
-    
-    st.write(USER_DATA['all']["text2"], unsafe_allow_html=True)
-    
+
     st.image("img/bersi2.png")
     
-    st.write(USER_DATA['all']["text3"], unsafe_allow_html=True)
-  
+    st.write(USER_DATA['all']["text2"], unsafe_allow_html=True)
+
     st.image("img/bersi3.png")
+    
+    st.write(USER_DATA['all']["text3"], unsafe_allow_html=True)
 
-    st.write(USER_DATA['all']["text4"], unsafe_allow_html=True)
-
-    maps_url12 = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.6972540616753!2d10.019785576637224!3d45.63681782190421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478165bef8329e71%3A0xe84fbfd81f60c13b!2sBersi%20Serlini%20Franciacorta!5e0!3m2!1sen!2sit!4v1740606188935!5m2!1sen!2sit"
+    maps_url2 = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2789.6972540616753!2d10.019785576637224!3d45.63681782190421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478165bef8329e71%3A0xe84fbfd81f60c13b!2sBersi%20Serlini%20Franciacorta!5e0!3m2!1sen!2sit!4v1740606188935!5m2!1sen!2sit"
 
     # Iframe responsive con larghezza dinamica
     st.markdown(
@@ -46,8 +44,7 @@ def app():
 
 # Dizionario con messaggi e immagini personalizzate
 USER_DATA = {
-    "all": {"text1": "La cerimonia nella sala civica del **Comune di Passirano**.",
-            "text2": "Il ricevimento si terrà nell'incantevole cantina **Bersi Serlini**, situata nel cuore della Franciacorta a **Provaglio d\'Iseo** (BS).",
+    "all": {"text1": "La cerimonia si terrà nella sala civica del **Comune di Passirano** alle ore **11.30**.<br>",
+            "text2": "A seguire, per il ricevimento, ci sposteremo nell'incantevole cantina **Bersi Serlini**, situata nel cuore della Franciacorta a **Provaglio d\'Iseo** (BS).",
            "text3": "I festeggiamenti inizieranno alle ore **12.30** e dureranno fino alle ore **20.30**.",
-           "text4": "Le indicazioni per raggiungerci.",},
 }
